c73758de953af0a37d7aab9ab2d73e919633d6c7
muddy700 <mohamedmfaume700@gmail.com>
2022-06-03 20:01:07 +0300

    feat: Add seeder for testing user

--- src/config/seeder.ts
+++ src/config/seeder.ts
@@ -1,6 +1,6 @@
 import { constants } from "./constants";
 import { seedUser } from "../modules/accessControl/seeder/";
-import { seedTestingUsers } from "../modules/users/seeder/index";
+import { seedTestingUser } from "../modules/users/seeder/index";
 import { seedInitialRoles } from "../modules/accessControl/seeder/role.seeder";
 
 export const seedInitialData = async () => {
@@ -10,10 +10,9 @@ export const seedInitialData = async () => {
     await seedUser();
 
     if (constants.NODE_ENV === "dev") {
-      // await seedTestingUsers(3);
-      // await seedTestingDeals(3)
-
       await seedInitialRoles();
+
+      await seedTestingUser();
     }
   } catch (e) {
     return {
--- src/modules/users/seeder/index.ts
+++ src/modules/users/seeder/index.ts
@@ -1,20 +1,31 @@
-import { faker } from "@faker-js/faker";
-import { User, IUser } from "../user.model";
 import { Role } from "../../accessControl/role.model";
+import { User, IUser, USER_TYPES } from "../user.model";
 
-export const seedTestingUsers = async (usersPerRole: number) => {
+export const seedTestingUser = async () => {
   try {
-    console.log("Seeding Test Users...");
+    console.log("Seeding Test User...");
 
-    const roles = await Role.find();
+    const role = await Role.findOne({ name: "System Tester" });
 
-    roles.forEach(async (role) => {
-      //   const usersPerRole = 3;
+    if (role) {
+      const count = await User.countDocuments({
+        email: systemTester.email,
+      });
 
-      if (role.name !== "Root") {
-        await createTestingUsers(role, usersPerRole);
+      if (count === 0) {
+        await User.create({ ...systemTester, role: role._id });
+
+        console.log(
+          "Created User: " + systemTester.email + " with role: " + role.name
+        );
+      } else {
+        console.log(
+          "User with email: " + systemTester.email + " already exist."
+        );
       }
-    });
+    } else {
+      console.log("No role found with name: System Tester");
+    }
 
     //Do other stuff
   } catch (e) {
@@ -22,55 +33,21 @@ export const seedTestingUsers = async (usersPerRole: number) => {
   }
 };
 
-export const createTestingUsers = async (role: any, usersPerRole: number) => {
-  try {
-    const users: Array<IUser> = [];
-
-    for (let i = 0; i < usersPerRole; i++) {
-      //Creating Dummy-Data depending on UsersPerRole
-
-      const userInfo = {
-        type: "staff",
-        role: role._id,
-        password: "12345678",
-        lastName: faker.name.lastName(),
-        firstName: faker.name.firstName(),
-        phoneNumber: faker.phone.phoneNumber(),
-        email: faker.internet.email().toLocaleLowerCase(),
-      } as unknown as IUser;
-
-      users.push(userInfo);
-    }
-
-    const count = await User.countDocuments({ role: role._id });
-
-    if (count < usersPerRole) {
-      users.forEach(async (user) => {
-        const emailExist = await User.countDocuments({ email: user.email });
-
-        if (emailExist === 0) {
-          await User.create(user);
-
-          console.log(
-            "Created User: " + user.email + " with role: " + role.name
-          );
-        } else {
-          console.log("User with email: " + user.email + " already exist.");
-        }
-      });
-
-      //Clear array
-      users.length = 0;
-    } else {
-      console.log(`You have: ${count} Users with role: ${role.name}.`);
-
-      //Clear array
-      users.length = 0;
-    }
-  } catch (e) {
-    return errorResponse(e);
-  }
-};
+/**
+ * System tester.
+ *
+ * User for testing api-endpoints
+ * in development and staging environments
+ */
+export const systemTester: IUser = {
+  status: 1,
+  lastName: "Tester",
+  firstName: "System",
+  password: "12345678",
+  type: USER_TYPES.USER,
+  email: "tester@mail.com",
+  phoneNumber: "255789101112",
+} as unknown as IUser;
 
 export const errorResponse = (error: any) => {
   //Formatting Response
