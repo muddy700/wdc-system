2d2dbf1bac91c4288456471a2babff2d1ed764f6
muddy700 <mohamedmfaume700@gmail.com>
2022-06-06 18:55:52 +0300

    fix: Comment project seeder

--- src/config/seeder.ts
+++ src/config/seeder.ts
@@ -2,7 +2,7 @@ import { constants } from "./constants";
 // import { seedTestingPius } from "../modules/pius/seeder";
 import { seedTestingUser } from "../modules/users/seeder";
 import { seedUser } from "../modules/accessControl/seeder/";
-import { seedTestingProjects } from "../modules/projects/seeder";
+// import { seedTestingProjects } from "../modules/projects/seeder";
 import { seedInitialRoles } from "../modules/accessControl/seeder/role.seeder";
 
 export const seedInitialData = async () => {
